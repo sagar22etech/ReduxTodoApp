a9005c9cdc89728b00731915bde661e691acd7a2
sagar_chand <sagar_chand@excellencetechnologies.in>
2018-07-28 15:12:49 +0530

    resolve the commented issues

--- src/home.js
+++ src/home.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class Home extends React.Component{
     render(){
-        return(<h1 style={{"textAlign":"center"}}>Welcome To The ToDo App</h1>)
+        return(<h1>Welcome To The ToDo App</h1>)
     }
 }
